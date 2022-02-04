087d1897e0ff8128888aa7d33009805b90ef913a
Raghav Dhingra <raghav.dhingra15@gmail.com>
2022-02-04 18:33:36 +0530

    updated help section

--- src/components/help/help.jsx
+++ src/components/help/help.jsx
@@ -17,7 +17,16 @@ const Help = () => {
           );
         })}
       </ol>
-      <p>For more information, you can refer to </p>
+      <p className="help-section-paragraph">
+        For more information, you can refer to{" "}
+        <a
+          href="https://medium.com/@raghav.dhingra15/personal-pinterest-with-uploadcare-chrome-extension-5ec00de74c2e"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get Started
+        </a>
+      </p>
     </>
   );
 };
